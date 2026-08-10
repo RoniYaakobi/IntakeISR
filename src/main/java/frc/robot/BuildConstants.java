41659e81b83f1dd07b0b4cb1d8a82f8146aad8f9
Roni <roniyaakobi@yahoo.com>
2026-08-10 23:25:31 +0300

    Lol lets not talk about it

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "IntakeISR";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "8de3aa17f725dd033f30c6a2d0a9ecd30669a1f8";
-  public static final String GIT_DATE = "29-07-2026 19:54:59 GMT+02:00";
+  public static final int GIT_REVISION = 4;
+  public static final String GIT_SHA = "45ee1b59fce515f1adb9aaa22752d509b4d2baf4";
+  public static final String GIT_DATE = "10-08-2026 22:15:52 GMT+02:00";
   public static final String GIT_BRANCH = "Framework";
-  public static final String BUILD_DATE = "10-08-2026 13:31:59 GMT+02:00";
-  public static final long BUILD_UNIX_TIME = 1786361519092L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "10-08-2026 22:15:56 GMT+02:00";
+  public static final long BUILD_UNIX_TIME = 1786392956374L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,6 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import io.github.captainsoccer.basicmotor.motorManager.MotorManager;
 
 public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
@@ -39,7 +38,6 @@ public class Robot extends LoggedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
-    MotorManager.getInstance().periodic();
   }
 
   @Override
